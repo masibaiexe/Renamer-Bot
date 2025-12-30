b7eb75fbf10bf2af733ec18e8c094ebfb974beec
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-12-30 09:55:55 +0300

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,6 +1,6 @@
-#  Telegram MTProto API Client Library for Pyrogram
+#  Telegram MTProto API Client Library for Telethon
 #  Copyright (C) 2017-present DigitalBotz <https://github.com/DigitalBotz>
-#  I am a telegram bot, I created it using pyrogram library. https://github.com/pyrogram
+#  I am a telegram bot, I created it using Telethon library.
 """
 Apache License 2.0
 Copyright (c) 2022 @Digital_Botz
@@ -30,7 +30,7 @@ __version__ = "3.1.0"
 __license__ = " Apache License, Version 2.0"
 __copyright__ = "Copyright (C) 2022-present Digital Botz <https://github.com/DigitalBotz>"
 __programer__ = "<a href=https://github.com/DigitalBotz/Digital-Rename-Bot>Digital Botz</a>"
-__library__ = "<a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>"
+__library__ = "<a href=https://docs.telethon.dev/en/stable/>Tᴇʟᴇᴛʜᴏɴ</a>"
 __language__ = "<a href=https://www.python.org/>Pyᴛʜᴏɴ 3</a>"
 __database__ = "<a href=https://cloud.mongodb.com/>Mᴏɴɢᴏ DB</a>"
 __developer__ = "<a href=https://t.me/Digital_Botz>Digital Botz</a>"
@@ -39,36 +39,29 @@ __maindeveloper__ = "<a href=https://t.me/RknDeveloper>RknDeveloper</a>"
 # main copyright herders (©️)
 # I have been working on this repo since 2022
 
-
-# main working files 
-# - bot.py
-# - web_support.py
-# - plugins/
-# - start_&_cb.py
-# - Force_Sub.py
-# - admin_panel.py
-# - file_rename.py
-# - metadata.py
-# - prefix_&_suffix.py
-# - thumb_&_cap.py
-# - config.py
-# - utils.py
-# - database.py
-
-# bot run files
-# - bot.py
-# - Procfile
-# - Dockerfile
-# - requirements.txt
-# - runtime.txt
-
 from plugins.force_sub import not_subscribed, forces_sub, handle_banned_user_status
-from pyrogram import Client, filters
+from telethon import events
+from config import Config
 
-@Client.on_message(filters.private)
-async def _(bot, message):
-    await handle_banned_user_status(bot, message)
+# Only register handlers if the BOT instance is initialized (avoids errors during initial import)
+if hasattr(Config, 'BOT') and Config.BOT:
     
-@Client.on_message(filters.private & filters.create(not_subscribed))
-async def forces_sub_handler(bot, message):
-    await forces_sub(bot, message)
+    # Handler 1: Banned User Status
+    # Checks every incoming private message
+    @Config.BOT.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+    async def banned_check_handler(event):
+        # We pass the bot instance and the event (message)
+        await handle_banned_user_status(Config.BOT, event)
+    
+    # Handler 2: Force Subscription
+    # Custom filter function to check if user is NOT subscribed
+    async def _force_sub_filter(event):
+        if not event.is_private:
+            return False
+        # Returns True if user is NOT subscribed (triggering the handler)
+        # Assumes not_subscribed is updated to accept (client, event)
+        return await not_subscribed(Config.BOT, event)
+
+    @Config.BOT.on(events.NewMessage(incoming=True, func=_force_sub_filter))
+    async def forces_sub_handler(event):
+        await forces_sub(Config.BOT, event)
